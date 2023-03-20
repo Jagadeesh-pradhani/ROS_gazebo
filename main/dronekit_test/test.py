ad33e82769b716a78be411822eec7c4a4a287fd0
Jagadeesh_D_P <97280653+Jagadeesh-pradhani@users.noreply.github.com>
2023-03-20 17:47:51 +0530

    Create test.py

---
+++ main/dronekit_test/test.py
@@ -0,0 +1,161 @@
+from dronekit import connect, VehicleMode, LocationGlobalRelative, LocationGlobal
+from pymavlink import mavutil
+import time
+import argparse  
+import math
+import rospy
+from geometry_msgs.msg import PoseStamped
+import numpy as np
+import cv2
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--connect', default='127.0.0.1:14550') #port you will get in second terminal, usually 14550. 14551
+args = parser.parse_args()
+
+# Connect to the Vehicle
+print ('Connecting to vehicle on: %s' % args.connect)
+vehicle = connect(args.connect, baud=921600, wait_ready=True, timeout=60) #make sure timeout>30 as ros needs time to connect
+
+
+def gps_callback(gps_data):
+    global current_location
+    current_location = gps_data.pose.position
+
+rospy.Subscriber('/mavros/global_position/local', PoseStamped, gps_callback)
+
+
+def arm_and_takeoff(aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print("Basic pre-arm checks")
+    # Don't let the user try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+    #  after Vehicle.simple_takeoff will execute immediately).
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:  # Trigger just below target alt.
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+
+def get_distance_metres(aLocation1, aLocation2):
+    """
+    Returns the ground distance in metres between two LocationGlobal objects.
+
+    This method is an approximation, and will not be accurate over large distances and close to the
+    earth's poles. It comes from the ArduPilot test code:
+    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
+    """
+    dlat = aLocation2.lat - aLocation1.lat
+    dlong = aLocation2.lon - aLocation1.lon
+    return math.sqrt((dlat * dlat) + (dlong * dlong)) * 1.113195e5
+
+
+def get_location_metres(original_location, dNorth, dEast):
+    """
+    Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the
+    specified `original_location`. The returned LocationGlobal has the same `alt` value
+    as `original_location`.
+
+    The function is useful when you want to move the vehicle around specifying locations relative to
+    the current vehicle position.
+
+    The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
+
+    For more information see:
+    http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
+    """
+    earth_radius = 6378137.0  # Radius of "spherical" earth
+    # Coordinate offsets in radians
+    dLat = dNorth / earth_radius
+    dLon = dEast / (earth_radius * math.cos(math.pi * original_location.lat / 180))
+
+    # New position in decimal degrees
+    newlat = original_location.lat + (dLat * 180 / math.pi)
+    newlon = original_location.lon + (dLon * 180 / math.pi)
+    if type(original_location) is LocationGlobal:
+        targetlocation = LocationGlobal(newlat, newlon, original_location.alt)
+    elif type(original_location) is LocationGlobalRelative:
+        targetlocation = LocationGlobalRelative(newlat, newlon, original_location.alt)
+    else:
+        raise Exception("Invalid Location object passed")
+
+    return targetlocation;
+
+
+def goto(dNorth, dEast, gotoFunction=vehicle.simple_goto):
+    """
+    Moves the vehicle to a position dNorth metres North and dEast metres East of the current position.
+
+    The method takes a function pointer argument with a single `dronekit.lib.LocationGlobal` parameter for
+    the target position. This allows it to be called with different position-setting commands.
+    By default it uses the standard method: dronekit.lib.Vehicle.simple_goto().
+
+    The method reports the distance to target every two seconds.
+    """
+
+    currentLocation = vehicle.location.global_relative_frame
+    targetLocation = get_location_metres(currentLocation, dNorth, dEast)
+    targetDistance = get_distance_metres(currentLocation, targetLocation)
+    gotoFunction(targetLocation)
+
+    # print "DEBUG: targetLocation: %s" % targetLocation
+    # print "DEBUG: targetLocation: %s" % targetDistance
+
+    while vehicle.mode.name == "GUIDED":  # Stop action if we are no longer in guided mode.
+        # print "DEBUG: mode: %s" % vehicle.mode.name
+        remainingDistance = get_distance_metres(vehicle.location.global_relative_frame, targetLocation)
+        print("Distance to target: ", remainingDistance)
+        if remainingDistance <= targetDistance * 0.5:  # Just below target, in case of undershoot.
+            print("Reached target")
+            break;
+        time.sleep(2)
+        
+
+def goto_location(to_lat, to_lon, alt):
+    currentLocation = vehicle.location.global_frame
+    target_Location = LocationGlobalRelative(to_lat, to_lon, alt)
+    targetDistance = get_distance_metres(currentLocation, target_Location)
+    vehicle.simple_goto(target_Location, groundspeed)
+
+    while vehicle.mode.name == "GUIDED":
+        # print "DEBUG: mode: %s" % vehicle.mode.name
+        remainingDistance = get_distance_metres(vehicle.location.global_frame, target_Location)
+        print("Distance to target: ", remainingDistance)
+        if remainingDistance <= 1:  # Just below target, in case of undershoot.
+            print("Reached target")
+            break;
+        time.sleep(2)
+        
+ 
+def my_mission():	
+	arm_and_takeoff(30)
+	time.sleep(2)
+	goto_location(-35.3631471, 149.1652304, 30)
+
+my_mission()
+
+print("Close vehicle object")
+vehicle.close()
+
+print("Completed")
